63b6a9c9c7b1805c02046d28e038daed58239eea
Soomin Song <song.soomin.418@outlook.com>
2025-04-15 14:58:49 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿/// 2. Write a C# Sharp program that takes userid
+/// and password as input (type string). After 3
+/// wrong attempts, user will be rejected.
+
+uint badAttempts = 0;
+string userid="admin";
+string password = "password";
+
+while (badAttempts<3) {
+    Console.Write("User ID: ");
+    string idAttempt = Console.ReadLine();
+    Console.Write("Password: ");
+    string passwordAttempt = Console.ReadLine();
+    if(userid!=idAttempt || password!=passwordAttempt) {
+        badAttempts++;
+        Console.WriteLine($"Incorrect attempt #{badAttempts}");
+        if(badAttempts>=3) {
+            Console.WriteLine("Too many login attempts");
+        }
+    }
+}
