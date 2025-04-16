3e1e7dfe36c2e1742c5e3276b82e3cf8da539edd
Soomin Song <song.soomin.418@outlook.com>
2025-04-16 12:45:29 -0700

    Hash function implemented

--- Program.cs
+++ Program.cs
@@ -2,25 +2,83 @@
 /// and password as input (type string). After 3
 /// wrong attempts, user will be rejected.
 
-uint badAttempts = 0;
-string userid="admin";
-string password = "password";
-bool loginSuccess = false;
+using System.Security.Cryptography;
+using System.Text;
 
-while (badAttempts<3) {
+// initialize accounts
+Dictionary<string, string> userInfoDb = new Dictionary<string, string>
+{
+    { "admin", ComputeSha256Hash("password") },
+    { "soomin", ComputeSha256Hash("123456")}
+};
+
+// begin login loop
+bool loginSuccess = false;
+uint badAttempts = 0;
+while (badAttempts < 3)
+{
     Console.Write("User ID: ");
     string idAttempt = Console.ReadLine();
+    if (idAttempt == null) { idAttempt = ""; }
+
     Console.Write("Password: ");
     string passwordAttempt = Console.ReadLine();
-    if(userid!=idAttempt || password!=passwordAttempt) {
+    if (passwordAttempt == null) { passwordAttempt = ""; }
+    
+    string pwOnRecord;
+
+    try
+    {
+        // check if ID exists and get the password from dictionary
+        pwOnRecord = userInfoDb[idAttempt];
+    }
+    catch
+    {
+        // throw new Exception("ID not found");
+        Console.WriteLine("ID not found.");
+        pwOnRecord = "";
+    }
+
+    if (pwOnRecord == "")
+    {
+        // if ID is not found, do nothing and go back to asking for id/pw
+    }
+    else if (pwOnRecord != ComputeSha256Hash(passwordAttempt))
+    {
         badAttempts++;
         Console.WriteLine($"Incorrect attempt #{badAttempts}");
-        if(badAttempts>=3) {
+        if (badAttempts >= 3)
+        {
             Console.WriteLine("Too many login attempts");
         }
-    } else {
-        loginSuccess=true;
+    }
+    else
+    {
+        loginSuccess = true;
         break;
     }
 }
-if(loginSuccess) {Console.WriteLine("Login successful!");}
+if (loginSuccess)
+{
+    // main menu and whatnot here
+    Console.WriteLine("Login successful!");
+}
+
+/// https://www.c-sharpcorner.com/article/compute-sha256-hash-in-c-sharp/
+static string ComputeSha256Hash(string rawData)
+{
+    // Create a SHA256
+    using (SHA256 sha256Hash = SHA256.Create())
+    {
+        // ComputeHash - returns byte array
+        byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+        // Convert byte array to a string
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < bytes.Length; i++)
+        {
+            builder.Append(bytes[i].ToString("x2"));
+        }
+        return builder.ToString();
+    }
+}
